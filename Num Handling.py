5cfcf4331d5794c9ff2cd0920232482a86e7cc02
Tanvir Shaikh <sktanvir9834@gmail.com>
2026-02-28 17:02:03 +0530

    My Assignments PUSH

---
+++ Num Handling.py
@@ -0,0 +1,12 @@
+try:
+    num1=int(input())
+    num2=int(input())
+    print("sum is:-",num1+num2)
+
+    if num2==0:
+        print("Cannot Divide By Zero")
+    else:
+        print("Division Result:-",num1/num2)
+
+except ValueError:
+    print("Invalid Input")
